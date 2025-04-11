252840ea17c70ea16feae0fcc45e0eae928fed07
hongduck710 <hongik710@gmail.com>
2025-04-11 20:24:39 +0900

    로그인 기능까지 구현!

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,6 +3,9 @@ import {toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {Link, useNavigate} from "react-router-dom";
 import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import ArrowRightIcon from "../assets/svg/keyboardArrowRightIcon.svg";
+
 //import OAuth from "../compoents/OAuth"; // 추후에 
 
 const SignIn = () => {
@@ -23,10 +26,61 @@ const SignIn = () => {
 
     const onSubmit = async (e) => {
         e.preventDefault();
+        try {
+            const auth = getAuth();
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            if(userCredential.user) {
+                navigate("/");
+            }
+        } catch(error){
+            toast.error("Bad User Credentials", error);
+        }
     }
 
     return (
-        <h2>로그인부터</h2>
+        <>
+            <div className="pageContainer">
+                <header><p className="pageHeader">어서오세요!</p></header>
+                <main>
+                    <form onSubmit={onSubmit}>
+                        <input 
+                        type="email" 
+                        className="emailInput"
+                        placeholder="Email"
+                        id="email"
+                        value={email}
+                        onChange={onChange}
+                        />
+                        <div className="passwordInputDiv">
+                            <input 
+                            type={showPassword ? "text" : "password"} 
+                            className="passwordInput"
+                            placeholder="password" 
+                            id="password"
+                            value={password}
+                            onChange={onChange}
+                            />
+                            <img 
+                            src={visibilityIcon}
+                            className="showPassword"
+                            alw="show password"
+                            onClick={() => setShowPassword((prevState) => !prevState)}
+                            />
+
+                            <Link to="/forgot-password" className="forgotPasswordLink">
+                                Forgot Password
+                            </Link>
+                            <div className="signInBar">
+                                <p className="signInText">Sign In</p>
+                                <button className="signInButton">
+                                    <img src={ArrowRightIcon} />
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </main>
+            </div>
+        </>
     );
 }
 export default SignIn;
